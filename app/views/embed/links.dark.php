232239bea335b6ca15cf2f4044cf2196ef0bafdd
Wolfy-J <wolfy.jd@gmail.com>
2019-09-07 15:35:55 +0300

    - disable worker limit in rr - enabled metrics service

--- app/views/embed/links.dark.php
+++ app/views/embed/links.dark.php
@@ -1,4 +1,5 @@
 <a href="${git}" style="${style}" target="_blank">[[GitHub]]</a>
 | <a href="/exception.html">[[Exception]]</a>
 | <a href="/ping.html">[[Create Queue Task]]</a>
+| <a href="http://localhost:2112/metrics">[[Application Metrics]]</a>
 | <a href="http://spiral-framework.com" target="_blank">[[Framework Website]]</a>
