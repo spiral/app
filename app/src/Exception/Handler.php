2d632b6c7ad19e8ff4549c2451a368c3db365914
butschster <butschster@gmail.com>
2022-06-16 12:48:07 +0400

    Adds Logger reporter to Exception handler

--- app/src/Exception/Handler.php
+++ app/src/Exception/Handler.php
@@ -7,18 +7,22 @@ namespace App\Exception;
 use Spiral\Exceptions\ExceptionHandler;
 use Spiral\Exceptions\Renderer\ConsoleRenderer;
 use Spiral\Exceptions\Renderer\JsonRenderer;
+use Spiral\Exceptions\Reporter\LoggerReporter;
 use Spiral\Exceptions\Reporter\SnapshotterReporter;
 
 final class Handler extends ExceptionHandler
 {
-    public function __construct(SnapshotterReporter $reporter)
-    {
+    public function __construct(
+        SnapshotterReporter $snapshotterReporter,
+        LoggerReporter $loggerReporter
+    ) {
         parent::__construct();
 
         // $this->addRenderer(new ConsoleRenderer());
         $this->addRenderer(new CollisionRenderer());
         $this->addRenderer(new JsonRenderer());
 
-        $this->addReporter($reporter);
+        $this->addReporter($loggerReporter);
+        $this->addReporter($snapshotterReporter);
     }
 }
