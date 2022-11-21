b797dad77f046c39536d21129e7f8d63854b6525
kastahov <kastahov1021@gmail.com>
2022-11-21 16:35:29 +0200

    Update grpc config

--- app/config/grpc.php
+++ app/config/grpc.php
@@ -7,26 +7,26 @@ return [
      * Path, where generated DTO files put.
      * Default: null
      */
-    'generatedPath' => directory('root') . '/app',
+    // 'generatedPath' => directory('root') . '/app',
 
     /**
      * Base namespace for generated proto files.
      * Default: null
      */
-    'namespace' => '\GRPC',
+    // 'namespace' => '\GRPC',
 
     /**
      * Root path for all proto files in which imports will be searched.
      * Default: null
      */
-    'servicesBasePath' => directory('root') . '/proto',
+    // 'servicesBasePath' => directory('root') . '/proto',
 
     /**
      * Path to protoc-gen-php-grpc library.
      * You can download the binary here: https://github.com/roadrunner-server/roadrunner/releases
      * Default: null
      */
-    'binaryPath' => directory('root').'/bin/protoc-gen-php-grpc',
+    // 'binaryPath' => directory('root').'/bin/protoc-gen-php-grpc',
 
     /**
      * Paths to proto files, that should be compiled into PHP by "grpc:generate" console command.
