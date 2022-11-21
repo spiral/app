1d246435f878f78593cb28fc8d669c506123b4ce
Kirill Astakhov <kastahov1021@gmail.com>
2022-11-21 18:43:20 +0200

    Added `generatedPath`, `servicesBasePath` and `namespace` to grpc config

--- app/config/grpc.php
+++ app/config/grpc.php
@@ -4,11 +4,29 @@ declare(strict_types=1);
 
 return [
     /**
+     * Path, where generated DTO files put.
+     * Default: null
+     */
+    // 'generatedPath' => directory('root') . '/app',
+
+    /**
+     * Base namespace for generated proto files.
+     * Default: null
+     */
+    // 'namespace' => '\GRPC',
+
+    /**
+     * Root path for all proto files in which imports will be searched.
+     * Default: null
+     */
+    // 'servicesBasePath' => directory('root') . '/proto',
+
+    /**
      * Path to protoc-gen-php-grpc library.
      * You can download the binary here: https://github.com/roadrunner-server/roadrunner/releases
      * Default: null
      */
-    // 'binaryPath' => directory('root').'protoc-gen-php-grpc',
+    // 'binaryPath' => directory('root').'/bin/protoc-gen-php-grpc',
 
     /**
      * Paths to proto files, that should be compiled into PHP by "grpc:generate" console command.
