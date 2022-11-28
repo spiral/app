d767ecfeb697ff1874c89a11c2a057f3a9545d5c
Maxim Smakouz <m.smakouz@gmail.com>
2022-11-28 14:31:40 +0200

    Add spiral/validator Bootloaders generator

---
+++ installer/Package/Generator/SpiralValidator/Bootloaders.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Installer\Package\Generator\SpiralValidator;
+
+use Installer\Package\Generator\Context;
+use Installer\Package\Generator\GeneratorInterface;
+use Spiral\Bootloader\Security\FiltersBootloader;
+use Spiral\Validation\Bootloader\ValidationBootloader;
+use Spiral\Validator\Bootloader\ValidatorBootloader;
+
+final class Bootloaders implements GeneratorInterface
+{
+    public function process(Context $context): void
+    {
+        $context->kernel->loadAppend(ValidationBootloader::class, FiltersBootloader::class);
+        $context->kernel->loadAppend(ValidatorBootloader::class, ValidationBootloader::class);
+    }
+}
