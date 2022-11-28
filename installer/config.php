71e4e9c74ca03964822fbe8b4d662f7f600eb965
Maxim Smakouz <m.smakouz@gmail.com>
2022-11-28 19:22:13 +0200

    Add psalm config for each application skeleton

--- installer/config.php
+++ installer/config.php
@@ -20,28 +20,43 @@ return [
         resources: [
             'applications/web/app.php' => 'app.php',
             'applications/web/deptrac.yaml' => 'deptrac.yaml',
+            'applications/web/psalm.xml' => 'psalm.xml',
             'applications/web/app' => 'app',
             'applications/web/public' => 'public',
             'applications/web/tests' => 'tests',
         ]
     ),
-    new Application\Cli(questions: [
-        new Question\CycleBridge(),
-    ]),
-    new Application\GRPC(),
-    new Application\CustomBuild(questions: [
-        new Question\ExtMbString(),
-        new Question\Psr7Implementation(),
-        new Question\Sapi(),
-        new Question\RoadRunnerBridge(),
-        new Question\RoadRunnerGRPC(),
-        new Question\CycleBridge(),
-        new Question\CycleCollections(),
-        new Question\TemporalBridge(),
-        new Question\TemplateEngine(),
-        new Question\Validator(),
-        new Question\EventBus(),
-        new Question\Scheduler(),
-        new Question\SentryBridge(),
-    ]),
+    new Application\Cli(
+        questions: [
+            new Question\CycleBridge(),
+        ],
+        resources: [
+            'applications/cli/psalm.xml' => 'psalm.xml',
+        ]
+    ),
+    new Application\GRPC(
+        resources: [
+            'applications/grpc/psalm.xml' => 'psalm.xml',
+        ]
+    ),
+    new Application\CustomBuild(
+        questions: [
+            new Question\ExtMbString(),
+            new Question\Psr7Implementation(),
+            new Question\Sapi(),
+            new Question\RoadRunnerBridge(),
+            new Question\RoadRunnerGRPC(),
+            new Question\CycleBridge(),
+            new Question\CycleCollections(),
+            new Question\TemporalBridge(),
+            new Question\TemplateEngine(),
+            new Question\Validator(),
+            new Question\EventBus(),
+            new Question\Scheduler(),
+            new Question\SentryBridge(),
+        ],
+        resources: [
+            'applications/custom/psalm.xml' => 'psalm.xml',
+        ]
+    ),
 ];
