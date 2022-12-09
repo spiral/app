e39bf3648be3afbdc0d7c17d1781cf67c63da735
kastahov <kastahov1021@gmail.com>
2022-12-09 18:43:37 +0200

    Replace Kernel constants with functions

--- app/src/Application/Kernel.php
+++ app/src/Application/Kernel.php
@@ -22,101 +22,109 @@ use Spiral\Views\Bootloader\ViewsBootloader;
 
 class Kernel extends \Spiral\Framework\Kernel
 {
-    protected const SYSTEM = [
-        CoreBootloader::class,
-        TokenizerBootloader::class,
-        DotEnv\DotenvBootloader::class,
-    ];
+    protected function defineSystemBootloaders(): array
+    {
+        return [
+            CoreBootloader::class,
+            TokenizerBootloader::class,
+            DotEnv\DotenvBootloader::class,
+        ];
+    }
 
     /*
      * List of components and extensions to be automatically registered
      * within system container on application start.
      */
-    protected const LOAD = [
+    protected function defineBootloaders(): array
+    {
+        return [
+            // Logging and exceptions handling
+            Monolog\MonologBootloader::class,
+            Bootloader\ExceptionHandlerBootloader::class,
 
-        // Logging and exceptions handling
-        Monolog\MonologBootloader::class,
-        Bootloader\ExceptionHandlerBootloader::class,
+            // Application specific logs
+            Bootloader\LoggingBootloader::class,
 
-        // Application specific logs
-        Bootloader\LoggingBootloader::class,
+            // RoadRunner
+            RoadRunnerBridge\CacheBootloader::class,
+            // RoadRunnerBridge\GRPCBootloader::class,
+            RoadRunnerBridge\HttpBootloader::class,
+            RoadRunnerBridge\QueueBootloader::class,
 
-        // RoadRunner
-        RoadRunnerBridge\CacheBootloader::class,
-        // RoadRunnerBridge\GRPCBootloader::class,
-        RoadRunnerBridge\HttpBootloader::class,
-        RoadRunnerBridge\QueueBootloader::class,
+            // Core Services
+            Framework\SnapshotsBootloader::class,
+            Framework\I18nBootloader::class,
 
-        // Core Services
-        Framework\SnapshotsBootloader::class,
-        Framework\I18nBootloader::class,
+            // Security and validation
+            Framework\Security\EncrypterBootloader::class,
+            ValidationBootloader::class,
+            Framework\Security\FiltersBootloader::class,
+            Framework\Security\GuardBootloader::class,
 
-        // Security and validation
-        Framework\Security\EncrypterBootloader::class,
-        ValidationBootloader::class,
-        Framework\Security\FiltersBootloader::class,
-        Framework\Security\GuardBootloader::class,
+            // HTTP extensions
+            Nyholm\NyholmBootloader::class,
+            Framework\Http\RouterBootloader::class,
+            Framework\Http\JsonPayloadsBootloader::class,
+            Framework\Http\CookiesBootloader::class,
+            Framework\Http\SessionBootloader::class,
+            Framework\Http\CsrfBootloader::class,
+            Framework\Http\PaginationBootloader::class,
+            SapiBootloader::class,
 
-        // HTTP extensions
-        Nyholm\NyholmBootloader::class,
-        Framework\Http\RouterBootloader::class,
-        Framework\Http\JsonPayloadsBootloader::class,
-        Framework\Http\CookiesBootloader::class,
-        Framework\Http\SessionBootloader::class,
-        Framework\Http\CsrfBootloader::class,
-        Framework\Http\PaginationBootloader::class,
-        SapiBootloader::class,
+            // Databases
+            CycleBridge\DatabaseBootloader::class,
+            CycleBridge\MigrationsBootloader::class,
+            // CycleBridge\DisconnectsBootloader::class,
 
-        // Databases
-        CycleBridge\DatabaseBootloader::class,
-        CycleBridge\MigrationsBootloader::class,
-        // CycleBridge\DisconnectsBootloader::class,
+            // ORM
+            CycleBridge\SchemaBootloader::class,
+            CycleBridge\CycleOrmBootloader::class,
+            CycleBridge\AnnotatedBootloader::class,
+            CycleBridge\CommandBootloader::class,
 
-        // ORM
-        CycleBridge\SchemaBootloader::class,
-        CycleBridge\CycleOrmBootloader::class,
-        CycleBridge\AnnotatedBootloader::class,
-        CycleBridge\CommandBootloader::class,
+            // DataGrid
+            // CycleBridge\DataGridBootloader::class,
 
-        // DataGrid
-        // CycleBridge\DataGridBootloader::class,
+            // Auth
+            // CycleBridge\AuthTokensBootloader::class,
 
-        // Auth
-        // CycleBridge\AuthTokensBootloader::class,
+            // Entity checker
+            // CycleBridge\ValidationBootloader::class,
 
-        // Entity checker
-        // CycleBridge\ValidationBootloader::class,
-
-        // Views and view translation
-        ViewsBootloader::class,
-        Framework\Views\TranslatedCacheBootloader::class,
+            // Views and view translation
+            ViewsBootloader::class,
+            Framework\Views\TranslatedCacheBootloader::class,
 
 
-        // Extensions and bridges
-        Stempler\StemplerBootloader::class,
+            // Extensions and bridges
+            Stempler\StemplerBootloader::class,
 
-        // Framework commands
-        Framework\CommandBootloader::class,
-        Scaffolder\ScaffolderBootloader::class,
+            // Framework commands
+            Framework\CommandBootloader::class,
+            Scaffolder\ScaffolderBootloader::class,
 
-        // Debug and debug extensions
-        Framework\DebugBootloader::class,
-        Framework\Debug\LogCollectorBootloader::class,
-        Framework\Debug\HttpCollectorBootloader::class,
+            // Debug and debug extensions
+            Framework\DebugBootloader::class,
+            Framework\Debug\LogCollectorBootloader::class,
+            Framework\Debug\HttpCollectorBootloader::class,
 
-        RoadRunnerBridge\CommandBootloader::class,
+            RoadRunnerBridge\CommandBootloader::class,
 
-        Bootloader\RoutesBootloader::class,
-    ];
+            Bootloader\RoutesBootloader::class,
+        ];
+    }
 
     /*
      * Application specific services and extensions.
      */
-    protected const APP = [
-        // fast code prototyping
-        Prototype\PrototypeBootloader::class,
+    protected function defineAppBootloaders(): array
+    {
+        return [
+            // fast code prototyping
+            Prototype\PrototypeBootloader::class,
 
-        // Modules
-        \App\Module\EmptyModule\Application\Bootloader::class,
-    ];
+            // Modules
+            \App\Module\EmptyModule\Application\Bootloader::class,
+        ];
+    }
 }
