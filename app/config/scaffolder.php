bf908a576cfd71813d2ec0b8b0a687bbc7d49ab8
Maxim Smakouz <m.smakouz@gmail.com>
2023-04-11 10:40:18 +0300

    Update scaffolder config

--- app/config/scaffolder.php
+++ app/config/scaffolder.php
@@ -2,9 +2,34 @@
 
 declare(strict_types=1);
 
+use Spiral\Scaffolder\Declaration;
+
 /**
  * @see \Spiral\Scaffolder\Config\ScaffolderConfig
  */
 return [
     'namespace'    => 'App',
+    'declarations' => [
+        Declaration\BootloaderDeclaration::TYPE => [
+            'namespace' => 'Application\\Bootloader',
+        ],
+        Declaration\ConfigDeclaration::TYPE => [
+            'namespace' => 'Application\\Config',
+        ],
+        Declaration\ControllerDeclaration::TYPE => [
+            'namespace' => 'Api\\Web\\Controller',
+        ],
+        Declaration\FilterDeclaration::TYPE => [
+            'namespace' => 'Api\\Web\\Filter',
+        ],
+        Declaration\MiddlewareDeclaration::TYPE => [
+            'namespace' => 'Application\\Middleware',
+        ],
+        Declaration\CommandDeclaration::TYPE => [
+            'namespace' => 'Api\\Cli\\Command',
+        ],
+        Declaration\JobHandlerDeclaration::TYPE => [
+            'namespace' => 'Api\\Job',
+        ],
+    ],
 ];
