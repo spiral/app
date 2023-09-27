f2e916742bc2adf258612c3271c1828d08c4ec99
shoman4eg <askew.39rus@gmail.com>
2023-09-27 02:14:21 +0200

    fix: use import, see QueueInterface namespace

--- installer/Module/Queue/resources/app/src/Endpoint/Job/Ping.php
+++ installer/Module/Queue/resources/app/src/Endpoint/Job/Ping.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Endpoint\Job;
 
-use Installer\Module\RoadRunnerBridge\resources\grpc\app\src\Endpoint\Console\PingCommand;
+use App\Endpoint\Console\PingCommand;
 use Psr\Log\LoggerInterface;
 use Spiral\Queue\JobHandler;
 
@@ -12,7 +12,7 @@ use Spiral\Queue\JobHandler;
  * Simple job handler that will be invoked by a queue consumer.
  * To push a job to a queue use {@see PingCommand} console command.
  *
- * Or use {@see Spiral\Queue\QueueInterface} directly.
+ * Or use {@see \Spiral\Queue\QueueInterface} directly.
  * (Spiral\Queue\QueueInterface)->push(PingJob::class, ["site"=>"http://site.com"]);
  */
 final class Ping extends JobHandler
