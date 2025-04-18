8ad444b67cb5afaa523272d77c4c246e63ccc4df
roxblnfk <roxblnfk@ya.ru>
2025-04-19 01:48:08 +0400

    Disable PostgresDriverConfig and MySQLDriverConfig if related extensions are not installed

--- installer/Module/CycleBridge/resources/config/database.php
+++ installer/Module/CycleBridge/resources/config/database.php
@@ -58,35 +58,39 @@ return [
                 'withDatetimeMicroseconds' => env('DB_WITH_DATETIME_MICROSECONDS', false),
             ],
         ),
-        'pgsql' => new Config\PostgresDriverConfig(
-            connection: new Config\Postgres\TcpConnectionConfig(
-                database: env('DB_DATABASE', 'spiral'),
-                host: env('DB_HOST', '127.0.0.1'),
-                port: (int) env('DB_PORT', 5432),
-                user: env('DB_USERNAME', 'postgres'),
-                password: env('DB_PASSWORD', ''),
+        ...(\extension_loaded('pdo_pgsql') ? [
+            'pgsql' => new Config\PostgresDriverConfig(
+                connection: new Config\Postgres\TcpConnectionConfig(
+                    database: env('DB_DATABASE', 'spiral'),
+                    host: env('DB_HOST', '127.0.0.1'),
+                    port: (int) env('DB_PORT', 5432),
+                    user: env('DB_USERNAME', 'postgres'),
+                    password: env('DB_PASSWORD', ''),
+                ),
+                schema: env('DB_SCHEMA', 'public'),
+                queryCache: env('DB_QUERY_CACHE', true),
+                options: [
+                    'logQueryParameters' => env('DB_LOG_QUERY_PARAMETERS', false),
+                    'withDatetimeMicroseconds' => env('DB_WITH_DATETIME_MICROSECONDS', false),
+                ],
+            )
+        ] : []),
+        ...(\extension_loaded('pdo_mysql') ? [
+            'mysql' => new Config\MySQLDriverConfig(
+                connection: new Config\MySQL\TcpConnectionConfig(
+                    database: env('DB_DATABASE', 'spiral'),
+                    host: env('DB_HOST', '127.0.0.1'),
+                    port: (int) env('DB_PORT', 3307),
+                    user: env('DB_USERNAME', 'root'),
+                    password: env('DB_PASSWORD', ''),
+                ),
+                queryCache: env('DB_QUERY_CACHE', true),
+                options: [
+                    'logQueryParameters' => env('DB_LOG_QUERY_PARAMETERS', false),
+                    'withDatetimeMicroseconds' => env('DB_WITH_DATETIME_MICROSECONDS', false),
+                ],
             ),
-            schema: env('DB_SCHEMA', 'public'),
-            queryCache: env('DB_QUERY_CACHE', true),
-            options: [
-                'logQueryParameters' => env('DB_LOG_QUERY_PARAMETERS', false),
-                'withDatetimeMicroseconds' => env('DB_WITH_DATETIME_MICROSECONDS', false),
-            ],
-        ),
-        'mysql' => new Config\MySQLDriverConfig(
-            connection: new Config\MySQL\TcpConnectionConfig(
-                database: env('DB_DATABASE', 'spiral'),
-                host: env('DB_HOST', '127.0.0.1'),
-                port: (int) env('DB_PORT', 3307),
-                user: env('DB_USERNAME', 'root'),
-                password: env('DB_PASSWORD', ''),
-            ),
-            queryCache: env('DB_QUERY_CACHE', true),
-            options: [
-                'logQueryParameters' => env('DB_LOG_QUERY_PARAMETERS', false),
-                'withDatetimeMicroseconds' => env('DB_WITH_DATETIME_MICROSECONDS', false),
-            ],
-        ),
+        ] : []),
         // ...
     ],
 ];
